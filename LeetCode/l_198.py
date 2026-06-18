1d5b41e4484de4579265d83c445335009eee70e2
goelaryan001 <goel.aryan001@fmail.com>
2026-06-19 05:22:24 +0530

    leetcode practice added

--- LeetCode/l_198.py
+++ LeetCode/l_198.py
@@ -1,9 +1,14 @@
-# Brute: we have a decision tree type like which house to rob, lets say we rob house 1 , then 2 cnt be which leads to decision of choosing from subprob house 3 and 4 to get max or second scenario we chose house 2 as starting then we have only option of house 4
+# Brute: we have a decision tree type like which house to rob, lets say we rob house 1 , 
+# then 2 cnt be which leads to decision of choosing from subprob house 3 and 4 to get max 
+# or second scenario we chose house 2 as starting then we have only option of house 4
 
 # rob=max(array[0]+rob[2,n], rob[1,n])
 
 #     1, 2, 3, 1
-#SUM: 1 | 2| 4| 3 or 4 so 4 so here the concept is we are calculating subprob like just looking the prev 2, like if 4th one is selected  then we have to look up the answer of of subprob of 1 and 2 which is 2, then 1+2 is 3 whereas if we dont select house 4 then it is solution of the subarray before that which is already calculated as 4
+#SUM: 1 | 2| 4| 3 or 4 so 4 so here the concept is we are calculating subprob like just looking 
+# the prev 2, like if 4th one is selected  then we have to look up the answer of of subprob 
+# of 1 and 2 which is 2, then 1+2 is 3 whereas if we dont select house 4 then it is solution of 
+# the subarray before that which is already calculated as 4
 #dp[i]=max(nums[i]+dp[i-2],dp[i-1])
 
 class Solution:
