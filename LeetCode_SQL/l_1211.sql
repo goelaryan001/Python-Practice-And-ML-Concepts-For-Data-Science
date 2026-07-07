043c962510b60a98b7e7800d1d70b46c5c9fc4bc
goelaryan001 <goel.aryan001@fmail.com>
2026-07-07 22:48:07 +0530

    leetcode sql practice addded

---
+++ LeetCode_SQL/l_1211.sql
@@ -0,0 +1,10 @@
+-- Write your PostgreSQL query statement below
+SELECT
+    query_name,
+    ROUND(AVG(rating::numeric / position), 2) AS quality,
+    ROUND(
+        100.0 * AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END),
+        2
+    ) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name;
