1f79a2ec165c39cef360bd63b4530c6ef42fa887
goelaryan001 <goel.aryan001@fmail.com>
2026-07-16 10:24:22 +0530

    leetcode sql practice addded

--- LeetCode_SQL/l_197.sql
+++ LeetCode_SQL/l_197.sql
@@ -3,3 +3,16 @@ from Weather w1
 join Weather w2
 on w1.recordDate=w2.recordDate + Interval '1 day'
 where w1.temperature>w2.temperature
+
+with cte as
+(select
+id,
+temperature,
+recordDate,
+LAG(recordDate) OVER (ORDER BY recordDate) AS prev_date,
+lag(temperature) over (order by recordDate) as prev
+from Weather) 
+
+select id from cte
+where temperature > prev
+and recordDate=prev_date+interval '1 day'
