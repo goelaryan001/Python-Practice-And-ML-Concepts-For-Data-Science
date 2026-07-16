0a687df084714ef8fc72551eb1a9aa89a8ab4d16
goelaryan001 <goel.aryan001@fmail.com>
2026-07-16 10:07:47 +0530

    leetcode sql practice addded

--- LeetCode_SQL/l_182.sql
+++ LeetCode_SQL/l_182.sql
@@ -3,3 +3,12 @@ Select email as Email
 from Person
 Group by email 
 having count(distinct id)>1
+
+SELECT DISTINCT email AS Email
+FROM (
+    SELECT
+        email,
+        COUNT(*) OVER (PARTITION BY email) AS cnt
+    FROM Person
+) t
+WHERE cnt > 1;
