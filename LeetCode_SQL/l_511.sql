d5c080f649734f40a555618cd449b8b1e9601567
goelaryan001 <goel.aryan001@fmail.com>
2026-07-16 10:38:52 +0530

    leetcode sql practice addded

--- LeetCode_SQL/l_511.sql
+++ LeetCode_SQL/l_511.sql
@@ -2,3 +2,11 @@ select
 player_id, min(event_date) as first_login
 from activity
 group by player_id
+
+SELECT DISTINCT
+    player_id,
+    FIRST_VALUE(event_date) OVER (
+        PARTITION BY player_id
+        ORDER BY event_date
+    ) AS first_login
+FROM Activity;
