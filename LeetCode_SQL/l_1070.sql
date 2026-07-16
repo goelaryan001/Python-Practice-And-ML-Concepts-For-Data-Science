5cf652ad94c8e5da644f23ab791d231564d7c9e0
goelaryan001 <goel.aryan001@fmail.com>
2026-07-16 11:02:32 +0530

    leetcode sql practice addded

--- LeetCode_SQL/l_1070.sql
+++ LeetCode_SQL/l_1070.sql
@@ -9,3 +9,15 @@ inner join cte
 aon s.product_id=cte.product_id
 and s.year=cte.first_year
 
+
+with cte as(select
+product_id, 
+year,
+first_value(year) over (partition by product_id order by year) as first_year,
+quantity, price
+from Sales)
+
+select product_id, 
+first_year,
+quantity, price
+from cte where year=first_year
