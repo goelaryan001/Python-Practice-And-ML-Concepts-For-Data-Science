821498db46ffe2d97e2bbcca621388a57aab9f87
goelaryan001 <goel.aryan001@fmail.com>
2026-07-16 10:05:00 +0530

    leetcode sql practice addded

--- LeetCode_SQL/l_180.sql
+++ LeetCode_SQL/l_180.sql
@@ -2,3 +2,15 @@ Select distinct l1.num as ConsecutiveNums
 From Logs l1
 Join Logs l2 on l1.id=l2.id-1 and l1.num=l2.num
 Join Logs l3 on l2.id=l3.id-1 and l2.num=l3.num
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM (
+    SELECT
+        id,
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev1,
+        LAG(num, 2) OVER (ORDER BY id) AS prev2
+    FROM Logs
+) t
+WHERE num = prev1
+  AND num = prev2;
